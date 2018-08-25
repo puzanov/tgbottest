badc77e8695a602bbc2ba4f08126d67702f77b42
Oleg Puzanov <puzanov@gmail.com>
2018-08-25 17:31:27 +0600

    pass bot name via cli arg

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3.6
 
+import sys
 from tgintegration import BotIntegrationClient
+
+if len(sys.argv) !=2: 
+    raise Exception("Need to pass bot name via cli args. Example: ./test.py @my_awesome_bot")
 
 c = BotIntegrationClient(
         session_name='my_account',
-        bot_under_test='@namba_taxi_bot',  # We're going to test the @BotListBot
-        max_wait_response=4,  # Wait a max of 10 seconds for responses, ...
+        bot_under_test=sys.argv[1],
+        max_wait_response=4,  # Wait a max of 4 seconds for responses, ...
         raise_no_response=False,  # ... then check for response.empty instead of raising
         min_wait_consecutive=2.0,  # Wait at least 2 seconds to collect more than one message
         global_action_delay=1.8,  # Space out all messages by 1.8 seconds
